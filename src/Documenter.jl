6ab22847e78ed29bcb3d0d7bd1e123b32c912882
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-04-10 13:59:21 +0100

    add decoding back in

--- src/Documenter.jl
+++ src/Documenter.jl
@@ -573,7 +573,7 @@ function git_push(
 
         keyfile = abspath(joinpath(root, ".documenter"))
         try
-            write(keyfile, documenter_key(deploy_config))
+            write(keyfile, base64decode(documenter_key(deploy_config)))
         catch e
             @error """
             Documenter failed to decode the DOCUMENTER_KEY environment variable.
